9df41e81e59de520a53b004aade6d77fb23dfbe5
ayub <109924893+ayub01zero@users.noreply.github.com>
2024-06-24 00:07:55 +0300

    add method

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,5 +4,12 @@ namespace App\Http\Controllers;
 
 class ApiController extends Controller
 {
-    //
+    
+    public function index()
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Welcome to the API'
+        ]);
+    }
 }
