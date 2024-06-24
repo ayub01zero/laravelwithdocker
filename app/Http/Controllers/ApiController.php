701fcfb1d418566b25681b0013540da144bb12ba
ayub01zero <ayubm.mohammed01@gmail.com>
2024-06-24 16:46:19 +0300

    new messsage from developer

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,4 +14,20 @@ class ApiController extends Controller
             'message' => 'Welcome to the API'
         ]);
     }
+
+    public function getPosts()
+    {
+        $posts = [
+            ['id' => 1, 'title' => 'Post 1', 'body' => 'This is the body of post 1'],
+            ['id' => 2, 'title' => 'Post 2', 'body' => 'This is the body of post 2'],
+            ['id' => 3, 'title' => 'Post 3', 'body' => 'This is the body of post 3'],
+            ['id' => 4, 'title' => 'Post 4', 'body' => 'This is the body of post 4'],
+            ['id' => 5, 'title' => 'Post 5', 'body' => 'This is the body of post 5'],
+        ];
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $posts
+        ]);
+    }
 }
